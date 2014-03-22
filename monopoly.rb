0f8d0019dafbe54b17a52a03661803bdd89678fb
chuckgreiner <chuckjohn@tamu.edu>
2014-03-21 23:24:10 -0500

    update square class

--- monopoly.rb
+++ monopoly.rb
@@ -4,7 +4,8 @@ require_relative 'square'
 require_relative 'player'
 
 chuck = Player.new(1, "chuck")
-go = GoSquare.new(0, :none, "go", "bank")
-melrose = Square.new(1, :purple, "melrose place", "bank")
+go = GoSquare.new(0, "go")
+baltic = Purple.new(1, "baltic", 100, "bank")
 
 chuck.locate
+baltic.color
--- square.rb
+++ square.rb
@@ -4,21 +4,10 @@ class Square
   @@gameboard =  []
 
   #constructor method
-  def initialize(id, color, name, owner)
+  def initialize(id, name)
     @@gameboard << name
     @id = id
-    @color = color
     @name = name
-    @owner = owner
-  end
-
-  #general methods
-  def identify
-    puts "identifying #{@name}..."
-    sleep(1)
-    puts "square id: #{@id}"
-    puts "square color: #{@color}"
-    puts "square name: #{@name}"
   end
 
   #accessor methods
@@ -29,19 +18,75 @@ class Square
   def id
     puts @id
   end
-  def color
-    puts @color
-  end
   def name
     puts @name
-  end
-  def owner
-    puts @owner
   end
 end
 
 class GoSquare < Square
   def payup
     puts "player landed on go. player gets 200"
+  end
+end
+
+class CommunityChest < Square
+  def opensesame
+    puts "player opened community chest"
+  end
+end
+
+class Chance < Square
+  def chance
+    puts "player took a chance"
+  end
+end
+
+class Jail < Square
+  def incarceration
+    puts "player is in jail"
+  end
+end
+
+class FreeParking < Square
+  def moneytime
+    puts "player wins the money"
+  end
+end
+
+class GoToJail < Square
+  def arrest
+    puts "player goes to jail. player does not pass go."
+    puts "player does not collect 200. player does not drop the soap."
+  end
+end
+
+class Property < Square
+
+  #constructor method
+  def initialize(id, name, rent, owner)
+    @@gameboard << name
+    @id = id
+    @name = name
+    @rent = rent
+    @owner = owner
+    @color = "default"
+  end
+
+  #accessor methods
+  def color
+    puts @color
+  end
+  def rent
+    puts @rent
+  end
+  def owner
+    puts @owner
+  end
+end
+
+class Purple < Property
+  def color
+    @color = "purple"
+    puts @color
   end
 end

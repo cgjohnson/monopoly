544df91cf9e9b7fd0f8518e893214a5928810ac1
chuckgreiner <chuckjohn@tamu.edu>
2014-04-04 21:13:53 -0500

    add landlord.notify

--- monopoly.rb
+++ monopoly.rb
@@ -18,15 +18,40 @@ end
 module TurnRegulator
   def self.turn(players, squares)
     bank = Bank.new
+    landlord = LandLord.new
     localtruth = true
     while localtruth
       players.each do |player|
         PlayerMover.move(player, bank, squares)
+        landlord.notify(player, squares)
      end
     end
   end
 end
 
+class LandLord
+  def notify(player, squares)
+    @location = squares[player.position].name
+    @owner = squares[player.position].owner
+    @type = squares[player.position].type
+    @price = squares[player.position].price
+    @rent = squares[player.position].rent
+    @mortgage = squares[player.position].mortgage
+    if @type == "special"
+      puts "Information on #{@location}:"
+      puts "Type: #{@type}"
+      #need if statements handling varied outcomes
+      puts "something special happens!"
+    else
+      puts "Information on #{@location}:"
+      puts "Owned by: #{@owner}"
+      puts "Type: #{@type}"
+      puts "Price: #{@price}"
+      puts "Rent: #{@rent}"
+      puts "Mortgage: #{@mortgage}"
+    end
+  end
+end
 
 
 monopoly = Monopoly.new

166ea358654cc678cc146511b767e2f09925ad7f
chuckgreiner <chuckjohn@tamu.edu>
2014-04-05 00:10:07 -0500

    work on verifymonopolizer

--- bank.rb
+++ bank.rb
@@ -15,7 +15,7 @@ class Bank
         puts "Shit, you landed on a square owned by #{owner}!"
         rent = rent[0]
         color = squares[player.position].type
-        if Verify.monopoly?(color, squares) #&& no houses or hotels
+        if VerifyMonopoly.monopoly?(color, squares) #&& no houses or hotels
           puts "Shit, #{owner} has a monopoly here!"
           puts 'Luckily for you, there are no houses or hotel.'
           rent *=2
@@ -23,35 +23,42 @@ class Bank
         puts "It appears you owe $#{rent} in rent!"
         player.cash -= rent
         landlord.cash += rent
-        puts "I'm afraid #{player.name} now only has $#{player.cash} in cash..."
+        puts "I'm afraid #{player.name}, you now only has $#{player.cash} in cash..."
         puts "#{landlord.name}, you now have $#{landlord.cash} in cash!"
       end
     end
+  end
 
-    def sell(player, squares, owner, location, price)
-      if owner == "the Bank"
-        puts "#{player.name}, you have $#{player.cash}," 
-        puts "would you like to purchase #{location} for $#{price}?"
-        response = gets.chomp
-        localtruth = true
-        while localtruth
-          if response.upcase() == "YES" || response.upcase() == "Y"
-            puts "Wonderful!"
-            player.cash -= price
-            puts "#{player.name}, your remaining cash is: #{player.cash}!"
-            squares[player.position].owner = player.name
-            puts "#{squares[player.position].owner}, you now own #{location}!"
-            localtruth = false
-          elsif response.upcase() == "NO" || response.upcase() == "N"
-            puts "No? How dissapointing..."
-            localtruth = false
-          else
-            puts "I'm afraid I don't understand..."
-            puts "Please answer yes or no"
-            response = gets.chomp
+  def sell(player, squares, owner, location, price)
+    if owner == "the Bank"
+      puts "#{player.name}, you have $#{player.cash}," 
+      puts "would you like to purchase #{location} for $#{price}?"
+      response = gets.chomp
+      localtruth = true
+      while localtruth
+        if response.upcase() == "YES" || response.upcase() == "Y"
+          puts "Wonderful!"
+          player.cash -= price
+          puts "#{player.name}, your remaining cash is: #{player.cash}!"
+          squares[player.position].owner = player.name
+          puts "#{squares[player.position].owner}, you now own #{location}!"
+          color = squares[player.position].type
+          if Verify.monopoly?(color, squares)
+            puts "#{player.name}, you now control the #{color} monopoly!"
           end
+          localtruth = false
+        elsif response.upcase() == "NO" || response.upcase() == "N"
+          puts "No? How dissapointing..."
+          localtruth = false
+        else
+          puts "I'm afraid I don't understand..."
+          puts "Please answer yes or no"
+          response = gets.chomp
         end
       end
     end
+  end
+  def monopolize
+    #something with VerifyMonopoilzer
   end
 end
--- verify.rb
+++ verify.rb
@@ -4,7 +4,7 @@ class Array
   end
 end
 
-module Verify
+module VerifyMonopoly
   def self.monopoly?(color, squares)
     #determine length of monopoly
     owners = []
@@ -15,5 +15,35 @@ module Verify
     end
     #determine if monopoly
     owners.same_values?
-    end
   end
+end
+
+
+### PROBABLY NEED TO REWORK THIS
+module VerifyMonopolizer
+  def self.monopolizer?(player, squares)
+    colors = ['purple',
+              'light blue',
+              'pink',
+              'orange',
+              'red',
+              'yellow',
+              'green',
+              'blue',
+              'utility',
+              'railroad']
+    monopolies = []
+    colors.each do |color|
+      if VerifyMonopoly.monopoly?(color, squares)
+        squares.each do |square|
+          if square.type == color && square.owner == player.name
+            monopolies << color
+          end
+        end
+        true
+      end
+    end
+        verified = monopolies.uniq
+        puts "#{player.name}, you have #{verified.length} monopolies: #{verified}"
+  end
+end
